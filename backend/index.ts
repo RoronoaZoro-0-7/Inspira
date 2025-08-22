ccec4941ce8df6d3135dc3e633cffbf67b54b423
Zoro07 <nadipallijaswanth541@gmail.com>
2025-08-22 15:43:49 +0530

    Finalised the schema

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,6 @@ dotenv.config();
 
 const app = express();
 
-app.listen(3000,() => {
-    console.log("Server is running on port 3000")
+app.listen(process.env.port,() => {
+    console.log(`Server is running on port ${process.env.port}`)
 })
